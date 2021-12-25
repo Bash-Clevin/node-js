4b801a6632328ed7484733eaea9d04034e6ff78a
Bash-Clevin <clevinbash@gmail.com>
2021-12-25 11:28:15 +0300

    working with modules

---
+++ 7-os-module.js
@@ -0,0 +1,17 @@
+const os = require('os')
+
+// current user info
+const user = os.userInfo()
+console.log(user)
+
+// system uptime in seconds
+console.log(`The system uptine is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMem:os.freemem()
+}
+
+console.log(currentOS)
