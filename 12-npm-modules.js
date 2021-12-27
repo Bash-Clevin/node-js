a5a096f13e0836589960795946e4fb000ee4819e
Bash-Clevin <clevinbash@gmail.com>
2021-12-27 06:17:52 +0300

    updated how to use npm and package.json

---
+++ 12-npm-modules.js
@@ -0,0 +1,15 @@
+// npm - global command, comes with node
+// npm i <packageName>          - install as a local depednency
+// npm install -g <packageName>  - install package as a global dependency
+
+// package.json         - manifest file that stores projeect/package info
+// installation
+// manual approach      - create package.json in root folder and add properties
+// npm init             - guided step by step
+// npm init -y          - everything will be set to default
+
+const _ = require('lodash')
+
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
