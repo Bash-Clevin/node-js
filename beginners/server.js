eac8a9e3397cc644e2b2518d969600e0e58d9059
kevin otieno <kevin.otieno@megatherium.solutions>
2022-07-25 20:22:20 +0300

    feat: working with modules

---
+++ beginners/server.js
@@ -0,0 +1,46 @@
+// import modules
+
+// common core modules
+const os = require('os')
+const path = require('path')
+
+// import custom modules
+
+/*  
+// 1. way
+
+const math = require('./math')
+
+console.log(math.add(2,3))
+ */
+
+
+/*  // 2. way
+//destructure the import
+
+const { add } = require('./math') 
+
+console.log(add(2,3))
+
+const { subtact, multiply, divide } = require('./math')
+
+console.log(subtract(2,3))
+console.log(multiply(2,3))
+console.log(divide(2,3))
+ */
+
+
+// console.log(os.type());
+// console.log(os.version());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename));
+
+// create our own modules
+
+
+
